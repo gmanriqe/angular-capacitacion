165a301a2551aca3c65aa7fd890531a0aad0a5ae
gmanriqe <gmanriqe@gmail.com>
2020-07-19 18:26:54 -0500

    Prueba avantica

---
+++ my-project/project-avantica/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MaterialModule } from './material/material.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    MaterialModule,
+    ClienteModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
